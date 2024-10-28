3869c307781cb200b329c62c00691a6c10e140b8
lucenabo <alucena1002@gmail.com>
2024-10-28 18:44:53 +0100

    Migration to Linux

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,9 +1,34 @@
+import {Link} from "react-router-dom";
+import {FaArrowLeft} from "react-icons/fa6";
 
-function Blog(){
-    return(
+function Blog() {
+    return (
         <div>
-            <h1>Blog</h1>
+            <nav style={{
+                display: 'flex',
+                position: 'fixed',
+                top: 0,
+                left: 0,
+                paddingTop: '5px',
+                paddingBottom: '5px',
+                paddingLeft: '20px',
+                width: '100%',
+                backgroundColor: 'black'
+            }}>
+                <Link to={"/"} className="text-4xl text-white"><FaArrowLeft/></Link>
+                <h1 style={{display: 'flex', justifyContent: 'center', width: '100%'}}
+                    className="text-4xl text-white">My Blog</h1>
+            </nav>
+            <main>
+                <section>
+
+                </section>
+                <section>
+
+                </section>
+            </main>
         </div>
+
     )
 }
 
